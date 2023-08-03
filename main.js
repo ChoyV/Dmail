ae37586374a2c4a2aed1942a7ef34f49887cd578
ChoyV <87896430+ChoyV@users.noreply.github.com>
2023-08-04 00:06:41 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,68 @@
+const ethers = require('ethers');
+const fs = require('fs');
+const { SocksProxyAgent } = require('socks-proxy-agent');
+require('dotenv').config();
+
+const privateKeysFile = 'private_keys.txt';
+const proxiesFile = 'proxy.txt';
+
+const sendMailFragment = {
+    type: "function",
+    name: "send_mail",
+    constant: false,
+    inputs: [
+        {
+            name: "to",
+            type: "string"
+        },
+        {
+            name: "subject",
+            type: "string"
+        }
+    ],
+    outputs: [],
+    stateMutability: "nonpayable"
+};
+
+async function interactWithContract(privateKey, proxyConfig) {
+    const proxy = new SocksProxyAgent(proxyConfig);
+    const provider = new ethers.providers.JsonRpcProvider(process.env.RPC, { agent: proxy, chainId: 324 });
+    const wallet = new ethers.Wallet(privateKey, provider);
+    const contract = new ethers.Contract(process.env.C_ADR, [sendMailFragment], wallet);
+
+    function generateRandomEmail() {
+        const randomString = Math.random().toString(36).substring(2, 10);
+        const email = `${randomString}@gmail.com`;
+        return email;
+    }
+
+    const to = generateRandomEmail();
+    const subj = 'test';
+    const gasLimit = 500000;
+
+    try {
+        const tx = await contract.send_mail(to, subj, { gasLimit });
+        const receipt = await tx.wait();
+        console.log('Success!\nHash:', receipt.transactionHash);
+    } catch (error) {
+        console.log('Error:', error);
+    }
+}
+
+async function main() {
+    const privateKeys = fs.readFileSync(privateKeysFile, 'utf8').trim().split('\n');
+    const proxies = fs.readFileSync(proxiesFile, 'utf8').trim().split('\n');
+
+    if (privateKeys.length !== proxies.length) {
+        console.error('Добавь одинаковое количество прокси и кошельков');
+        return;
+    }
+
+    for (let i = 0; i < privateKeys.length; i++) {
+        const privateKey = privateKeys[i].trim();
+        const proxyConfig = proxies[i].trim();
+        await interactWithContract(privateKey, proxyConfig);
+    }
+}
+
+main();
